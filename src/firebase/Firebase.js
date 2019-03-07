d0175e68f2a886595b6e0f67e8be53e68554332a
agarwal1995 <rahulagarwal199525@gmail.com>
2019-03-07 10:21:02 +0530

    Update Firebase.js

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -3,12 +3,12 @@
 import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyDVCF7F_KzH3ra-bPy0biz63IRNTxwT4Xc",
-  authDomain: "snappix-9d635.firebaseapp.com",
-  databaseURL: "https://snappix-9d635.firebaseio.com",
-  projectId: "snappix-9d635",
-  storageBucket: "snappix-9d635.appspot.com",
-  messagingSenderId: "46927983446"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 firebase.initializeApp(config);
